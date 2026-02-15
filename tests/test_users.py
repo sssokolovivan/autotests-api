c992488c1a041a4ba4a8474e5aa893a5809cb6f7
sssokolovivan <sssokolovivan@gmail.ru>
2026-02-15 21:01:38 +0300

    added markers

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,12 +1,15 @@
 from http import HTTPStatus
 
+import pytest
+
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response
 
-
+@pytest.mark.regression
+@pytest.mark.users
 def test_create_user():
     public_users_client = get_public_users_client()
 
